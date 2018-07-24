9c943b996b22fe5137fe59942287c54b072967bf
Jukiya Ogura <jukiya@JukiyanoMac-Pro.local>
2018-07-24 17:25:35 +0900

    no message

---
+++ exp/exp.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+import subprocess
+import json
+import requests
+import sys
+
+#辞書に登録（ファイル）
+filenames = ['Nasa.xml']
+
+#辞書に登録（問合せ）
+queries = { '//reference': 'axis:descendant($v, "reference")',
+					'//reference/child::source': 'axis:child(axis:descendant($v, "reference"), "source")',
+					'//reference/parent::*': 'axis:parent(axis:descendant($v, "reference"), "*")',
+					'//reference/follwing::tableHead': 'axis:following(axis:descendant($v, "reference"), "tableHead")',
+					'//author/ancestor::dataset': 'axis:ancestor(axis:descendant($v, "author"), "dataset")',
+					'//reference/child::source/child::other/child::title': 'axis:child(axis:child(axis:child(axis:descendant($v, "reference"), "source"), "other"), "title")',
+					'//field': 'axis:descendant($v, "field")'
+}
+
+def main ():
+	count = 0
+
+	for filename in filenames:
+		#辞書をループ
+		for query in queries:
+			count += 1
+
+			#ファイルの更新
+			file = open('../XQueryZ/main.xq', 'r+')
+			strings = file.read()
+			string = strings.split('(:===///===:)')
+			string[1] = queries[query]
+			strings = string[0] + '(:===///===:)\n' + string[1] + '(:===///===:)' + string[2]
+			file.write(strings)
+			file.close()
+
+			#実験
+			Signal(filename, query)
+			path = '../result/commandoutput' + str(count)
+			result = subprocess.check_output("./exp.sh")
+			file = open(path, 'w')
+			file.write(query + '\n\n' + result)
+			file.close()
+
+def Signal(file, query):
+
+	# Webhooks URL
+	url = "https://hooks.slack.com/services/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"
+
+	payload_dic = {
+    	"attachments": [
+    		{
+    			"title": file,
+   				"text": query
+    		}
+    	]
+	}
+
+	r = requests.post(url, data=json.dumps(payload_dic))
+
+
+
+if __name__ == '__main__':
+	main()
