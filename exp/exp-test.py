6bd1e234bcde53d8295d931c093e77accc47dbd3
小椋寿希也 <o-jukiya@ist.osaka-u.ac.jp>
2018-07-30 11:46:45 +0900

    test ver. 追加

---
+++ exp/exp-test.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+import subprocess
+import json
+import requests
+import sys
+
+#辞書に登録（ファイル）
+filenames = ['Nasa.xml']
+
+#辞書に登録（問合せ）
+queries = {
+					'//reference/self::*': 'axis:self(axis:descendant($v, "reference"), "*")',
+					'//reference/follwing::tableHead': 'axis:following(axis:descendant($v, "reference"), "tableHead")',
+					'//reference/preceding::tableHead': 'axis:preceding(axis:descendant($v, "reference"), "tableHead")',
+					'//reference/follwing-sibling::keyword': 'axis:following(axis:descendant($v, "reference"), "keyword")',
+					'//keyword/preceding-sibling::reference': 'axis:following(axis:descendant($v, "keyword"), "reference")',
+					'//author/ancestor::dataset': 'axis:ancestor(axis:descendant($v, "author"), "dataset")'
+			}
+
+def main ():
+	count = 0
+
+	for filename in filenames:
+		#辞書をループ
+		for query in queries:
+			count += 1
+
+			#ファイルの更新
+			file = open('../XQueryZ/main-test.xq', 'r')
+			strings = file.read()
+			file.close()
+			string = strings.split('(:===///===:)')
+			string[1] = queries[query]
+			strings = string[0] + '(:===///===:)\n' + string[1] + '\n(:===///===:)' + string[2]
+			file = open('../XQueryZ/main-test.xq', 'w')
+			file.write(strings)
+			file.close()
+
+			#実験
+			#Signal(filename, query)
+			path = '../result/commandoutput-test' + str(count)
+			result = subprocess.check_output(["./exp-test.sh", filename, query])
+			file = open(path, 'w')
+			file.write(str(query) + '\n\n' + str(result))
+			file.close()
+	else:
+		Signal('Finish', '')
+
+def Signal(file, query):
+
+	# Webhooks URL
+	url = "https://hooks.slack.com/services/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"
+
+	payload_dic = {
+    	"attachments": [
+    		{
+    			"title": file,
+   				"text": query
+    		}
+    	]
+	}
+
+	r = requests.post(url, data=json.dumps(payload_dic))
+
+
+
+if __name__ == '__main__':
+	main()
---
+++ exp/exp-test.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+
+minus_heap () {	# ヒープサイズを引く関数
+	heap=`expr $heap - $1`
+
+	./XQueryZ-test.sh "$heap"m "$stack"m
+
+	while [ "$?" -eq "0" ]
+	do
+		heap=`expr $heap - $1`
+		echo "$heap , $stack"
+		./XQueryZ-test.sh "$heap"m "$stack"m
+	done
+}
+
+minus_stack () {	# スタックサイズを引く関数
+	stack=`expr $stack - $1`
+
+	./XQueryZ-test.sh "$heap"m "$stack"m
+
+	while [ "$?" -eq "0" ]
+	do
+		stack=`expr $stack - $1`
+		echo "$heap , $stack"
+		./XQueryZ-test.sh "$heap"m "$stack"m
+	done
+}
+
+plus_heap () {	# ヒープサイズを足す関数
+	heap=`expr $heap + $1`
+
+	./XQueryZ-test.sh "$heap"m "$stack"m
+
+	while [ "$?" -eq "1" ]
+	do
+		heap=`expr $heap + $1`
+		echo "$heap , $stack"
+		./XQueryZ-test.sh "$heap"m "$stack"m
+	done
+}
+
+plus_stack () {	# スタックサイズを足す関数
+	stack=`expr $stack + $1`
+
+	./XQueryZ-test.sh "$heap"m "$stack"m
+
+	while [ "$?" -eq "1" ]
+	do
+		stack=`expr $stack + $1`
+		echo "$heap , $stack"
+		./XQueryZ-test.sh "$heap"m "$stack"m
+	done
+}
+
+filename="$1"
+query="$2"
+
+heap=120	# 初期ヒープサイズ指定
+stack=120	# 初期スタックサイズ指定
+
+./XQueryZ-test.sh "$heap"m "$stack"m
+
+if [ "$?" -eq "1" ]	# 実行失敗(1)したら
+then
+	plus_heap 100
+
+	minus_heap 10
+
+	plus_heap 1
+
+	plus_stack 100
+
+	minus_stack 10
+
+	plus_stack 1
+
+	python3 finish_signal.py "${filename}" "${query}" "$heap" "$stack"
+
+else	# 実行成功(0)したら
+	
+	minus_heap 100
+
+	plus_heap 10
+
+	minus_heap 1
+
+	heap=`expr $heap + 1`
+
+	minus_stack 100
+
+	plus_stack 10
+
+	minus_stack 1
+
+	stack=`expr $stack + 1` 
+
+	python3 finish_signal.py "${filename}" "${query}" "$heap" "$stack"
+
+fi
+
