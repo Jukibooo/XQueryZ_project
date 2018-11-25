19af9d0f36e8a53a44a2b8a7d858298774929265
小椋寿希也 <o-jukiya@ist.osaka-u.ac.jp>
2018-11-25 23:43:24 +0900

    no message

--- exp/XQueryZ.sh
+++ exp/XQueryZ.sh
@@ -1,3 +1,5 @@
 #java -Xmx"$1" -Xss"$2" -cp saxon9he.jar net.sf.saxon.Query -t -o:"../result/result.xml" -q:"../XQueryZ/main.xq"
 
-java -Xmx1g -Xss1g -cp saxon9he.jar net.sf.saxon.Query -t -o:"../result/result3.xml" -q:"../XQueryZ/main.xq"
+#java -Xmx1g -Xss1g -cp saxon9he.jar net.sf.saxon.Query -t -o:"../result/result3.xml" -q:"../XQueryZ/main.xq"
+
+basex -o "../result/result.xml" ../XQueryZ/XQueryZ.xq
