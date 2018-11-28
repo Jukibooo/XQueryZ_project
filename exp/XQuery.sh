2c03e0d8e657045be4083b6a8049588c639f86c9
Jukiya Ogura <jukiya@JukiyanoMac-Pro.local>
2018-11-28 14:37:53 +0900

    no message

--- exp/XQuery.sh
+++ exp/XQuery.sh
@@ -1,2 +1,2 @@
-java -Xmx1g -Xss1g -cp saxon9he.jar net.sf.saxon.Query -t -o:"../result/result.xml" -q:"../XQuery/XQuery.xq"
-#basex -o "../result/result.xml" ../XQuery/XQuery.xq
+#java -Xmx1g -Xss1g -cp saxon9he.jar net.sf.saxon.Query -t -o:"../result/result.xml" -q:"../XQuery/XQuery.xq"
+basex -o "../result/result.xml" ../XQuery/XQuery.xq
