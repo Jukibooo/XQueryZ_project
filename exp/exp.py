bcd0f65c935fca7e998a08c6b2dbbc7107523d37
Jukiya Ogura <jukiya@JukiyanoMac-Pro.local>
2019-02-07 14:24:15 +0900

    no message

--- exp/exp.py
+++ exp/exp.py
@@ -10,21 +10,21 @@ filenames = ['../ex/Lineitem/Lineitem-n.xml']
 
 #辞書に登録（問合せ）
 queries = {
-					#'$v/descendant::dataset/child::reference': 'axis:child(axis:descendant($v, "dataset"), "reference")',
-					#'$v/descendant::source/parent::reference': 'axis:parent(axis:descendant($v, "source"), "reference")',
-					#'$v/descendant::dataset/descendant::history': 'axis:descendant(axis:descendant($v, "dataset"), "history")',
-					#'$v/descendant::creator/ancestor::history': 'axis:ancestor(axis:descendant($v, "creator"), "history")',
-					#'$v/descendant::reference/following::tableLinks': 'axis:following(axis:descendant($v, "reference"), "tableLinks")',
-					#'$v/descendant::history/preceding::tableLinks': 'axis:preceding(axis:descendant($v, "history"), "tableLinks")',
-					#'$v/descendant::reference/following-sibling::tableHead': 'axis:following-sibling(axis:descendant($v, "reference"), "tableHead")',
-					#'$v/descendant::history/preceding-sibling::tableHead': 'axis:preceding-sibling(axis:descendant($v, "history"), "tableHead")',
-					#'$v/descendant::dataset/descendant-or-self::author': 'axis:descendant-or-self(axis:descendant($v, "dataset"), "author")',
-					#'$v/descendant::initial/ancestor::author': 'axis:ancestor(axis:descendant($v, "initial"), "author")'
+					'$v/descendant::dataset/child::reference': 'axis:child(axis:descendant($v, "dataset"), "reference")',
+					'$v/descendant::source/parent::reference': 'axis:parent(axis:descendant($v, "source"), "reference")',
+					'$v/descendant::dataset/descendant::history': 'axis:descendant(axis:descendant($v, "dataset"), "history")',
+					'$v/descendant::creator/ancestor::history': 'axis:ancestor(axis:descendant($v, "creator"), "history")',
+					'$v/descendant::reference/following::tableLinks': 'axis:following(axis:descendant($v, "reference"), "tableLinks")',
+					'$v/descendant::history/preceding::tableLinks': 'axis:preceding(axis:descendant($v, "history"), "tableLinks")',
+					'$v/descendant::reference/following-sibling::tableHead': 'axis:following-sibling(axis:descendant($v, "reference"), "tableHead")',
+					'$v/descendant::history/preceding-sibling::tableHead': 'axis:preceding-sibling(axis:descendant($v, "history"), "tableHead")',
+					'$v/descendant::dataset/descendant-or-self::author': 'axis:descendant-or-self(axis:descendant($v, "dataset"), "author")',
+					'$v/descendant::initial/ancestor::author': 'axis:ancestor(axis:descendant($v, "initial"), "author")',
 
 					#'$v/descendant::PLAYER/child::HOME_RUNS': 'axis:child(axis:descendant($v, "PLAYER"), "HOME_RUNS")'
 					#'$v/descendant::T/child::L_TAX': 'axis:child(axis:descendant($v, "T"), "L_TAX")'
 					#'$v/descendant::ACT/child::TITLE': 'axis:child(axis:descendant($v, "ACT"), "TITLE")'
-					#'$v/descendant::keywords/child::keyword': 'axis:child(axis:descendant($v, "keywords"), "keyword")'
+					'$v/descendant::keywords/child::keyword': 'axis:child(axis:descendant($v, "keywords"), "keyword")',
 					#'$v/descendant::VP/child::VBG': 'axis:child(axis:descendant($v, "VP"), "VBG")'
 					#'$v/descendant::proceedings/child::url' : 'axis:child(axis:descendant($v, "proceedings"), "url")' 
 
